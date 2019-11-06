18192008c38bc4ee1de3a60955d53f6bd0655c58
TeeMak <bunth667@gmail.com>
2019-11-05 21:12:29 -0500

    permutations

---
+++ permutations.js
@@ -0,0 +1,17 @@
+// in: [1,2,3]
+//
+//out: [
+//     [1,2,3]
+//     [1,3,2]
+//     [2,1,3]
+//     [2,3,1]
+//     [3,1,2]
+//     [3,2,1]
+//
+function permutations(arr) {
+	return arr;
+}
+
+let input = [1,2,3]
+let result = permutations(input);
+console.log(result);
