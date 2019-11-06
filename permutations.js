ef85f51bd398b5c1f43921ccca4c0c18482dca40
TeeMak <bunth667@gmail.com>
2019-11-05 21:37:01 -0500

    permuations

--- permutations.js
+++ permutations.js
@@ -9,9 +9,33 @@
 //     [3,2,1]
 //
 function permutations(arr) {
-	return arr;
+	const result = [];
+
+	const findPermutation = (visited = new Set(), current = []) => {
+		if (current.length === arr.length) {
+			result.push(current.join(""));
+			return;
+		}
+		for (let i = 0; i < arr.length; i++) {
+			if (!visited.has(i)) {
+				findPermutation(new Set([...visited, i]), [...current, arr[i]]);
+			}
+		}
+	};
+	findPermutation();
+	return result.forEach(word => console.log(word));
 }
 
-let input = [1,2,3]
-let result = permutations(input);
+let input = ["B", "A", "A", "M", "K", "E", "S", "S", "L"];
+// M A K
+//let input = ["B", "A", "E", "S", "S", "L"];
+// B L A K A M E S S
+// B L A K E M A S S
+// B L A K S M E S A
+// B A S E L M A S K
+//let input = ["A", "M", "L", "S", "S"];
+let sorted = input.sort();
+//console.log(sorted);
+//let input = [1, 2, 3];
+let result = permutations(sorted);
 console.log(result);
